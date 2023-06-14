3c20a941ae45d382487470fc11c5667ebb287318
Ana Gomes <paulinha0939@gmail.com>
2023-06-13 23:10:20 -0300

    01. A base de um Design System

---
+++ packages/tokens/src/colors.ts
@@ -0,0 +1,16 @@
+export const colors = {
+  white: '#fff',
+  black: '#000',
+
+  gray100: '#E1E1E6',
+  gray200: '#A9A9B2',
+  gray600: '#323238',
+  gray800: '#202024',
+  gray900: '#121214',
+  gray400: '#7C7C8A',
+
+  ignite300: '#00B37E',
+  ignite500: '#00875F',
+  ignite700: '#015F43',
+  ignite900: '#002910',
+}
